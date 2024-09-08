fe7541fddfbeb8e2605de5df701fe8fad1c69154
Davidcuama <74799027+Davidcuama@users.noreply.github.com>
2024-09-08 15:10:57 -0500

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,9 +1,13 @@
 import kareltherobot.*;
 import java.awt.Color;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
-import java.util.stream.Collectors;
+import java.util.Queue;
+import java.util.LinkedList;
+import java.util.Map;
+import java.util.HashMap;
+import java.util.Arrays;
+
 
 class Racer extends Robot {
     private int number;
@@ -16,13 +20,12 @@ class Racer extends Robot {
     private int startAvenue;
     private int beepersInTheBag;
 
-    public Racer(int street, int avenue, Direction direction, int beepers, Color color, int maxBeepers, String id,
-            int number) {
+    public Racer(int street, int avenue, Direction direction, int beepers, Color color, int maxBeepers, String id, int number) {
         super(street, avenue, direction, beepers, color);
         this.id = id;
         this.number = number;
         this.maxBeepers = maxBeepers;
-        this.deliveryPosition = new int[] { maxBeepers, 1 }; // Calle en la posición 0 y la avenida en la posición 1
+        this.deliveryPosition = new int[] { maxBeepers, 1 };
         this.currentStreet = street;
         this.currentAvenue = avenue;
         this.startStreet = street;
@@ -31,59 +34,43 @@ class Racer extends Robot {
         World.setupThread(this);
     }
 
-    public void returnToStart() {
-        System.out.println("Returning to start at (" + startStreet + ", " + startAvenue + ")");
-        moveToLocation(startStreet, startAvenue);
-        System.out.println("Returned to start at (" + startStreet + ", " + startAvenue + ")");
+    public void moveToLocation(int targetStreet, int targetAvenue, KarelWorldParser map) {
+        int[] start = {currentStreet, currentAvenue};
+        int[] goal = {targetStreet, targetAvenue};
+        List<int[]> path = map.bfs(start, goal);
+
+        if (path != null) {
+            for (int[] step : path) {
+                moveToExactLocation(step[0], step[1]);
+            }
+        }
     }
 
-    private boolean outOfBoundaries() {
-        return currentStreet < 1 || currentStreet > 8 || currentAvenue < 1 || currentAvenue > 10;
+    private void moveToExactLocation(int street, int avenue) {
+        moveToStreet(street);
+        moveToAvenue(avenue);
     }
 
     private void moveToStreet(int targetStreet) {
-        if (currentStreet < targetStreet) {
-            if (!facingNorth()) {
+        while (currentStreet != targetStreet) {
+            if (currentStreet < targetStreet) {
                 turnTo("NORTH");
-            }
-        } else if (currentStreet > targetStreet) {
-            if (!facingSouth()) {
+            } else {
                 turnTo("SOUTH");
             }
-        }
-
-        while (currentStreet != targetStreet) {
-            if (frontIsClear()) {
-                move();
-            } else {
-                break;
-            }
+            move();
         }
     }
 
     private void moveToAvenue(int targetAvenue) {
-        if (currentAvenue < targetAvenue) {
-            if (!facingEast()) {
+        while (currentAvenue != targetAvenue) {
+            if (currentAvenue < targetAvenue) {
                 turnTo("EAST");
-            }
-        } else if (currentAvenue > targetAvenue) {
-            if (!facingWest()) {
+            } else {
                 turnTo("WEST");
             }
+            move();
         }
-
-        while (currentAvenue != targetAvenue) {
-            if (frontIsClear()) {
-                move();
-            } else {
-                break;
-            }
-        }
-    }
-
-    private void moveToLocation(int targetStreet, int targetAvenue) {
-        moveToStreet(targetStreet);
-        moveToAvenue(targetAvenue);
     }
 
     private void turnTo(String direction) {
@@ -94,120 +81,48 @@ class Racer extends Robot {
 
     private boolean facingDirection(String direction) {
         switch (direction) {
-            case "NORTH":
-                return facingNorth();
-            case "SOUTH":
-                return facingSouth();
-            case "EAST":
-                return facingEast();
-            case "WEST":
-                return facingWest();
-            default:
-                return false;
+            case "NORTH": return facingNorth();
+            case "SOUTH": return facingSouth();
+            case "EAST":  return facingEast();
+            case "WEST":  return facingWest();
+            default: return false;
         }
-    }
-
-    public int getMaxBeepers() {
-        return this.maxBeepers;
-    }
-
-    public int[] getDeliveryPosition() {
-        return this.deliveryPosition;
-    }
-
-
-    // Obtener la ubicación actual
-    public int[] getCurrentPosition() {
-        return new int[] { currentStreet, currentAvenue };
-    }
-
-    // Actualizar la ubicación al moverse
-    private void updatePosition() {
-        if (facingNorth()) {
-            currentStreet++;
-        } else if (facingSouth()) {
-            currentStreet--;
-        } else if (facingEast()) {
-            currentAvenue++;
-        } else if (facingWest()) {
-            currentAvenue--;
-        }
-    }
-
-    // Mover y actualizar posición
-    @Override
-    public void move() {
-        super.move();
-        updatePosition();
     }
 }
 
 class RobotFactory implements Directions {
     public static Racer[] createRobots(String[] args) {
-        int r = 1;
-
-        // Crear robots
-        List<Color> colores = new ArrayList<>();
-        colores.add(Color.blue);
-        colores.add(Color.red);
-        colores.add(Color.green);
-        colores.add(Color.yellow);
-
-        List<String> colorNames = new ArrayList<>();
-        colorNames.add("blue");
-        colorNames.add("red");
-        colorNames.add("green");
-        colorNames.add("yellow");
-
-        int[] quantity_of_beepers = { 1, 2, 4, 8 };
-        Racer[] racers = new Racer[r];
-        int[] start_position = { 3, 5, 6, 7 };
-
-        for (int i = 0; i < r; i++) {
-            racers[i] = new Racer(1, start_position[i], East, 0, colores.get(i), quantity_of_beepers[i], colorNames.get(i),
-                    i);
+        List<Color> colores = Arrays.asList(Color.blue, Color.red, Color.green, Color.yellow);
+        Racer[] racers = new Racer[4];
+        for (int i = 0; i < 4; i++) {
+            racers[i] = new Racer(1, i + 1, East, 0, colores.get(i), 10, "Robot" + i, i);
         }
-
         return racers;
     }
 }
 
-
 class KarelWorldParser {
-    public String worldText;
     public boolean[][] matrix = new boolean[20][20];
 
     public KarelWorldParser(String worldText) {
+        parseWorld(worldText);
+    }
 
-        System.out.println("KarelWorldParser constructor");
-        
-        this.worldText = worldText;
-
-        for (int i = 0; i < 20; i++) {
-            for (int j = 0; j < 20; j++) {
-                matrix[i][j] = false;
-            }
-        }
-
+    private void parseWorld(String worldText) {
         String[] tokens = worldText.split(" ");
-
         for (int i = 0; i < tokens.length; i++) {
             if (tokens[i].equals("eastwestwalls")) {
                 int street = Integer.parseInt(tokens[i + 1]) - 1;
                 int avenue = Integer.parseInt(tokens[i + 2]) - 1;
                 fillMatrix(street, avenue);
-
             } else if (tokens[i].equals("northsouthwalls")) {
                 int avenue = Integer.parseInt(tokens[i + 1]) - 1;
                 int street = Integer.parseInt(tokens[i + 2]) - 1;
                 fillMatrix(street, avenue);
             }
         }
-
         printMatrix();
-
     }
-
 
     public void fillMatrix(int street, int avenue) {
         matrix[avenue][street] = true;
@@ -225,22 +140,57 @@ class KarelWorldParser {
             System.out.println();
         }
     }
+
+    public List<int[]> bfs(int[] start, int[] goal) {
+        int[][] directions = {{0, 1}, {0, -1}, {1, 0}, {-1, 0}};
+        boolean[][] visited = new boolean[20][20];
+        Queue<int[]> queue = new LinkedList<>();
+        Map<String, int[]> parent = new HashMap<>();
+
+        queue.add(start);
+        visited[start[0]][start[1]] = true;
+        parent.put(start[0] + "," + start[1], null);
+
+        while (!queue.isEmpty()) {
+            int[] current = queue.poll();
+            if (Arrays.equals(current, goal)) {
+                return reconstructPath(parent, current);
+            }
+
+            for (int[] dir : directions) {
+                int[] next = {current[0] + dir[0], current[1] + dir[1]};
+                if (isValidMove(next, visited)) {
+                    queue.add(next);
+                    visited[next[0]][next[1]] = true;
+                    parent.put(next[0] + "," + next[1], current);
+                }
+            }
+        }
+        return null;
+    }
+
+    private boolean isValidMove(int[] position, boolean[][] visited) {
+        return position[0] >= 0 && position[0] < 20 && position[1] >= 0 && position[1] < 20 &&
+               !visited[position[0]][position[1]] && !matrix[position[0]][position[1]];
+    }
+
+    private List<int[]> reconstructPath(Map<String, int[]> parent, int[] goal) {
+        List<int[]> path = new LinkedList<>();
+        for (int[] at = goal; at != null; at = parent.get(at[0] + "," + at[1])) {
+            path.add(0, at);
+        }
+        return path;
+    }
 }
-
-
 
 public class Main {
     public static void main(String[] args) {
         World.readWorld("Mundo.kwld");
         World.showSpeedControl(true);
         World.setVisible(true);
-        String worldText = World.asText(" ");
 
-        KarelWorldParser map = new KarelWorldParser(worldText);
-
+        KarelWorldParser map = new KarelWorldParser(World.asText(" "));
         Racer[] racers = RobotFactory.createRobots(args);
-
-        System.out.println("Start the searching:  " + '\n');
-
-}
+        racers[0].moveToLocation(10, 10, map); // ejemplo de robot utilizando la funcion BFS
+    }
 }
